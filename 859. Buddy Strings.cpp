2ddc928b37b881c9300ca22391a3522f99ef57df
unknown <cu.18bcs6591@gmail.com>
2023-07-03 22:52:59 +0530

    Added one problem from leetcode

---
+++ 859. Buddy Strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.length() != goal.length()) return false;
+        if(s==goal && set<char>(s.begin(),s.end()).size()<s.size()) return true;
+        vector<int> diff;
+        for(int i=0;i<s.length();++i){
+            if(s[i] != goal[i])
+                diff.push_back(i);
+        }
+        return diff.size() == 2 && s[diff[0]] == goal[diff[1]] && s[diff[1]] == goal[diff[0]];
+    }
+};
