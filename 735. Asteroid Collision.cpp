0f348ac424229ce61300fcb61ae64b9c4352fdb3
unknown <cu.18bcs6591@gmail.com>
2023-07-20 11:31:43 +0530

    Added one problem

---
+++ 735. Asteroid Collision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int n = asteroids.size();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(asteroids[i] > 0 || st.empty()){
+                st.push(asteroids[i]);
+            } else {
+                while(!st.empty() && st.top() > 0 && st.top() < abs(asteroids[i])){
+                    st.pop();
+                }
+                if(!st.empty() && st.top() == abs(asteroids[i])){
+                    st.pop();
+                } else{
+                    if(st.empty() || st.top() < 0){
+                        st.push(asteroids[i]);
+                    }
+                }
+            }
+        }
+        vector<int> res(st.size());
+        for(int i=(int)st.size()-1;i>=0;i--){
+            res[i] = st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
