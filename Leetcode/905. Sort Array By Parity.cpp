dd3cf092f67e11f20e5d7f5ac920bbd4357f54cd
unknown <cu.18bcs6591@gmail.com>
2023-09-28 10:12:52 +0530

    Added 1 Daily LC problem

---
+++ Leetcode/905. Sort Array By Parity.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0,j=0;j<nums.size();j++){
+            if(nums[j]%2 == 0) swap(nums[i++],nums[j]);
+        }
+        return nums;
+    }
+};
